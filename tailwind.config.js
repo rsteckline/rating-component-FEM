9a7e913d0c4eb130c560bc0684212a53bc3918cd
Bobby Steckline <rjsturing@gmail.com>
2024-03-24 09:11:21 -0400

    Implement TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      orange: "hsl(25, 97%, 53%)",
+      white: "hsl(0, 0%, 100%)",
+      "light-grey": "hsl(217, 12%, 63%)",
+      "medium-grey": "hsl(216, 12%, 54%)",
+      "dark-blue": "hsl(213, 19%, 18%)",
+      "very-dark-blue": "hsl(216, 12%, 8%)",
+    },
+    fontFamily: {
+      overpass: ["Overpass", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
