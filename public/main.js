597b72a0f0f46268e6f03c0188448f4ac39a8c1a
Bobby Steckline <rjsturing@gmail.com>
2024-03-25 09:32:01 -0400

    Add rating submission functionality

---
+++ public/main.js
@@ -0,0 +1,47 @@
+const ratingComponent = document.querySelector("#rating-component");
+const thankYouComponent = document.querySelector("#thank-you-component");
+const ratingButtons = document.querySelectorAll("input[name='rating']");
+const submitButton = document.querySelector("#submit-button");
+const selectedRatingSpan = document.querySelector("#selected-rating");
+
+let lastPressedRatingButton = null;
+
+ratingButtons.forEach((ratingButton) => {
+  ratingButton.addEventListener("click", (event) => {
+    if (
+      lastPressedRatingButton !== null &&
+      lastPressedRatingButton !== ratingButton
+    ) {
+      const lastPressedListItem = lastPressedRatingButton.parentElement;
+
+      lastPressedListItem.classList.add("bg-lighter-dark-blue");
+      lastPressedListItem.classList.remove("bg-medium-grey");
+      lastPressedListItem.classList.remove("text-white");
+    }
+
+    lastPressedRatingButton = ratingButton;
+
+    const listItem = ratingButton.parentElement;
+
+    listItem.classList.add("bg-medium-grey");
+    listItem.classList.remove("bg-lighter-dark-blue");
+    listItem.classList.add("text-white");
+  });
+});
+
+submitButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  const checkedRatingButton = document.querySelector(
+    "input[name='rating']:checked"
+  );
+
+  if (checkedRatingButton !== null) {
+    selectedRatingSpan.textContent = checkedRatingButton.value;
+
+    ratingComponent.classList.add("hidden");
+    ratingComponent.classList.remove("flex");
+
+    thankYouComponent.classList.add("flex");
+    thankYouComponent.classList.remove("hidden");
+  }
+});
