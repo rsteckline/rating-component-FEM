5105d63218de95b99832ff1788b7ba6af9c08471
Bobby Steckline <rjsturing@gmail.com>
2024-03-25 09:01:11 -0400

    Add thank you state styling and update package.json

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ["./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    screens: {
+      sm: "410px",
+    },
     colors: {
       orange: "hsl(25, 97%, 53%)",
       white: "hsl(0, 0%, 100%)",
@@ -9,13 +12,24 @@ module.exports = {
       "medium-grey": "hsl(216, 12%, 54%)",
       "dark-blue": "hsl(213, 19%, 18%)",
       "very-dark-blue": "hsl(216, 12%, 8%)",
+      "lighter-dark-blue": "hsl(213, 19%, 20%)",
     },
     fontFamily: {
       overpass: ["Overpass", "sans-serif"],
     },
     extend: {
       backgroundImage: {
-        "star-icon": 'url("images/icon-star.svg")',
+        "star-icon": 'url("assets/icon-star.svg")',
+      },
+      backgroundSize: {
+        "star-icon-size": "40%",
+      },
+      height: {
+        "card-mobile": "360px",
+        "card-desktop": "410px",
+      },
+      width: {
+        "card-desktop": "410px",
       },
     },
   },
